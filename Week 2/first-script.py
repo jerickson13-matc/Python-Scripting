cd63e3943fc41e0894ed968b996017007d7852a2
Jackerickson13 <jerickson13@madisoncollege.edu>
2022-06-28 17:10:51 -0500

    Ready for grading week 2 assignment first script

---
+++ Week 2/first-script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#Author: Jack Erickson
+#This is my first python script and will output some information I am learning in class
+
+myname = f"Jack Erickson"
+
+print(f"Hi, my name is {myname}")
+
+print("I leaned that you can get user input by using input()")
